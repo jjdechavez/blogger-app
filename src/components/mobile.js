11cc6992df0084504fdbccf647b4f38563b94203
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2019-10-31 21:47:46 +0800

    new viewport

--- src/components/mobile.js
+++ src/components/mobile.js
@@ -13,6 +13,11 @@ function App() {
             </div>
             <div className="navbar-menu">
               <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <defs>
+                  <style type="text/css">
+
+                  </style>
+                </defs>
                 <rect width="7.5" height="7.5" fill="white"/>
                 <rect y="11.25" width="7.5" height="7.5" fill="white"/>
                 <rect x="11.25" y="11.25" width="7.5" height="7.5" fill="white"/>

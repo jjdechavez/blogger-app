0d2f64816c8d8ac4ad627182af9e7c2cf7ae1154
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2019-11-04 21:51:56 +0800

    Add Style for Blog

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -5,23 +5,23 @@ function BlogItem() {
     <>
       <div className="item-container">
         <div className="item-grid">
-          <h5 className="about-title">Count Octagon</h5>
-          <p className="about-author">Friz Knuckle</p>
-          <p className="about-date">Feb 2019</p>
+          <h5 className="about-title">Sample Title</h5>
+          <p className="about-author">sample author</p>
+          <p className="about-date">new date</p>
         </div>
       </div>
       <div className="item-container2">
         <div className="item-grid">
-          <h5 className="about-title">Winter Vibe</h5>
-          <p className="about-author">Kose Side</p>
-          <p className="about-date">Aug 2019</p>
+          <h5 className="about-title">Sample Title</h5>
+          <p className="about-author">sample author</p>
+          <p className="about-date">new date</p>
         </div>
       </div>
       <div className="item-container3">
         <div className="item-grid">
-          <h5 className="about-title">New Anime</h5>
-          <p className="about-author">Weeb Man</p>
-          <p className="about-date">Sept 2019</p>
+          <h5 className="about-title">Sample Title</h5>
+          <p className="about-author">sample author</p>
+          <p className="about-date">new date</p>
         </div>
       </div>
     </>

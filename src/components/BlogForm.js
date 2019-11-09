7917c071fde85e9986fb21936d6c5748ab2ff885
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2019-11-09 22:08:18 +0800

    Finish addBlogForm

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -6,7 +6,7 @@ export const AddBlogForm = () => {
 
   const [title, setTitle] = useState('');
   const [blog, setBlog] = useState('');
-  
+
   const handleSubmit = e => {
     e.preventDefault();
     console.log(title, blog)
@@ -17,30 +17,37 @@ export const AddBlogForm = () => {
 
   return (
     <div className="form-container">
-      <div className="head-title">
-        <p>Add Blog :</p>
+      <div className="m-center">
+        <div className="head-title">
+          <p className="form-title">Add Blog :</p>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="form-row">
+            <label className="label">Title</label>
+            <input 
+              type="text"
+              value={title}
+              onChange={e => setTitle(e.target.value)}
+              placeholder="Title for the blog..."
+            />
+          </div>
+          <div className="form-row">
+            <label>Blog</label>
+            <textarea 
+              name="blog"
+              className="form-textarea"
+              cols="30" 
+              rows="10"
+              value={blog}
+              onChange={e => setBlog(e.target.value)} 
+            />
+          </div>
+          <div className="btn-container">
+            <button className="btn btn-save">Save</button>
+            <button className="btn btn-cancel">Cancel</button>
+          </div>
+        </form>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div className="form-row">
-          <label>Title</label>
-          <input 
-            type="text"
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-          />
-        </div>
-        <div className="form-row">
-          <label>Blog</label>
-          <textarea 
-            name="blog" 
-            cols="30" 
-            rows="10"
-            value={blog}
-            onChange={e => setBlog(e.target.value)} 
-          />
-        </div>
-        <button>Save</button>
-      </form>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,23 +10,27 @@ function Navbar() {
         </Link>
       </div>
       <div className="navbar-menu">
-        <svg className="menu-item d-none-desktop" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect width="7.5" height="7.5" fill="white"/>
-          <rect y="11.25" width="7.5" height="7.5" fill="white"/>
-          <rect x="11.25" y="11.25" width="7.5" height="7.5" fill="white"/>
-          <path d="M11.25 0H18.75V7.5H11.25V0Z" fill="white"/>
-        </svg>
-        <svg className="menu-item d-none-mobile" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect width="7.5" height="7.5" fill="black"/>
-          <rect y="11.25" width="7.5" height="7.5" fill="black"/>
-          <rect y="22.5" width="7.5" height="7.5" fill="black"/>
-          <rect x="11.25" y="22.5" width="7.5" height="7.5" fill="black"/>
-          <rect x="22.5" y="22.5" width="7.5" height="7.5" fill="black"/>
-          <rect x="11.25" y="11.25" width="7.5" height="7.5" fill="black"/>
-          <rect x="22.5" y="11.25" width="7.5" height="7.5" fill="black"/>
-          <path d="M11.25 0H18.75V7.5H11.25V0Z" fill="black"/>
-          <path d="M22.5 0H30V7.5H22.5V0Z" fill="black"/>
-        </svg>
+        <Link to="/add-blog-form">
+          <svg className="menu-item d-none-desktop" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="7.5" height="7.5" fill="white"/>
+            <rect y="11.25" width="7.5" height="7.5" fill="white"/>
+            <rect x="11.25" y="11.25" width="7.5" height="7.5" fill="white"/>
+            <path d="M11.25 0H18.75V7.5H11.25V0Z" fill="white"/>
+          </svg>
+        </Link>
+        <Link to="/add-blog-form">
+          <svg className="menu-item d-none-mobile" width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="7.5" height="7.5" fill="black"/>
+            <rect y="11.25" width="7.5" height="7.5" fill="black"/>
+            <rect y="22.5" width="7.5" height="7.5" fill="black"/>
+            <rect x="11.25" y="22.5" width="7.5" height="7.5" fill="black"/>
+            <rect x="22.5" y="22.5" width="7.5" height="7.5" fill="black"/>
+            <rect x="11.25" y="11.25" width="7.5" height="7.5" fill="black"/>
+            <rect x="22.5" y="11.25" width="7.5" height="7.5" fill="black"/>
+            <path d="M11.25 0H18.75V7.5H11.25V0Z" fill="black"/>
+            <path d="M22.5 0H30V7.5H22.5V0Z" fill="black"/>
+          </svg>
+        </Link>
       </div>
     </header>
   )
